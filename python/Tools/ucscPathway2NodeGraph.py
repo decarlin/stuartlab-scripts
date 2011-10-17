d850c2a5e34422c9f920e91cf02a167398413841
epaull <epaull@soe.ucsc.edu>
2011-10-17 14:25:00 -0700

    --

--- python/Tools/ucscPathway2NodeGraph.py
+++ python/Tools/ucscPathway2NodeGraph.py
@@ -9,6 +9,7 @@ parser.add_option("-p","--pathway_file",type="string",dest="pathway_file", actio
 parser.add_option("--tf_output",type="string",dest="tf_output", action="store", help="Output file for Transcription Factor links")
 parser.add_option("--c_output",type="string",dest="component_output", action="store", help="Output file for Component Map")
 parser.add_option("--ppi_output",type="string",dest="ppi_output", action="store", help="Output file for PPI links")
+parser.add_option("--flattened",type="string",dest="flattened", action="store", help="Join genes with all pathway links for complexes and families they belong to. Print that network with just proteins")
 (options, args) = parser.parse_args()
 
 
@@ -16,21 +17,25 @@ parser.add_option("--ppi_output",type="string",dest="ppi_output", action="store"
 #	name -> type
 # interactions:
 #	name -> interacting nodes
-nodes, Interactions = mPathway.rPathway(options.pathway_file, reverse = False, retProteins = False)
+nodes, Interactions, Proteins = mPathway.rPathway(options.pathway_file, reverse = False, retProteins = True)
 
 nodes, revInteractions = mPathway.rPathway(options.pathway_file, reverse = True, retProteins = False)
 # maps complex strings to the components in each
 componentMap = mPathway.getComponentMap(nodes, revInteractions)
 
-complexRE = re.compile(".*\(complex\).*")
+complexRE = re.compile(".*\((complex|family)\).*")
 
 # get the constituents of a complex
 def getGenes(complex_str, componentMap, depth):
 	all_genes = []
 
+	# give up
+	if complex_str not in componentMap:
+		return []
+
 	for component in componentMap[complex_str]:
 		if complexRE.match(component):
-			if component in componentMap and depth < 3:
+			if component in componentMap and depth < 4:
 				genes = getGenes(component, componentMap, depth+1)
 				for gene in genes:
 					all_genes.append(gene)
@@ -106,6 +111,63 @@ c_out = open(options.component_output, 'w')
 for component in componentMap:
 	c_out.write(component+"\t"+" ".join(componentMap[component])+"\n")
 c_out.close()
+
+# protein edges
+
+edges = {}
+for p in Proteins:
+
+	# build edges
+	edges[p] = {}
+
+	if not p in Interactions:
+		continue
+
+	# add direct interactions
+	for inode in Interactions[p]:
+		genes = None
+		if complexRE.match(inode):
+			genes = getGenes(inode, componentMap, 1)
+		else:
+			genes = [ inode ]
+
+		for gene in genes:
+			if p == gene:
+				continue
+			edges[p][gene] = None
+
+# go over every complex: for each gene
+for component in componentMap:
+	source_genes = getGenes(component, componentMap, 1)
+	sink_genes = {}
+
+	for node in componentMap[component]:
+		genes = None
+		if complexRE.match(inode):
+			genes = getGenes(node, componentMap, 1)
+		else:
+			genes = [ node ]
+
+		# connect these sink genes to the source gene
+		for sr in source_genes:
+
+			# only proteins here
+			if sr not in edges:
+				continue
+
+			for sink in genes:
+				# again, only proteins
+				if sink in edges:
+					if sr == sink:
+						continue
+					edges[sr][sink] = None
+
+# print it out
+flattened_out = open(options.flattened, 'w')
+for source in edges:
+	for sink in sorted(edges[source]):
+		flattened_out.write(source+"\t"+sink+"\n")
+flattened_out.close()
 
 #for source in complex_edges:
 #
