cdb48323c2570c37405cfa93bbf8c9ff8da20105
epaull <epaull@soe.ucsc.edu>
2011-12-19 16:01:48 -0800

    --greatly simplify the pathway flattening step. All interactions now include the interaction type as the third column. Many of these are complex or node membership. Proteins are not the only allowed entities: we should include all abstract processes and RNA or miRNA interactions.

--- python/Tools/ucscPathway2NodeGraph.py
+++ python/Tools/ucscPathway2NodeGraph.py
@@ -6,9 +6,7 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-p","--pathway_file",type="string",dest="pathway_file", action="store", help="UCSC Pathway File")
-parser.add_option("--tf_output",type="string",dest="tf_output", action="store", help="Output file for Transcription Factor links")
 parser.add_option("--c_output",type="string",dest="component_output", action="store", help="Output file for Component Map")
-parser.add_option("--ppi_output",type="string",dest="ppi_output", action="store", help="Output file for PPI links")
 parser.add_option("--flattened",type="string",dest="flattened", action="store", help="Join genes with all pathway links for complexes and families they belong to. Print that network with just proteins")
 (options, args) = parser.parse_args()
 
@@ -19,9 +17,9 @@ parser.add_option("--flattened",type="string",dest="flattened", action="store", 
 #	name -> interacting nodes
 nodes, Interactions, Proteins = mPathway.rPathway(options.pathway_file, reverse = False, retProteins = True)
 
-nodes, revInteractions = mPathway.rPathway(options.pathway_file, reverse = True, retProteins = False)
+rev_nodes, revInteractions = mPathway.rPathway(options.pathway_file, reverse = True, retProteins = False)
 # maps complex strings to the components in each
-componentMap = mPathway.getComponentMap(nodes, revInteractions)
+componentMap = mPathway.getComponentMap(rev_nodes, revInteractions)
 
 complexRE = re.compile(".*\((complex|family)\).*")
 
@@ -73,142 +71,50 @@ def addEdges(abs_source, abs_target, interaction, map, componentMap):
 		if (sr,interaction) not in map:
 			map[(sr,interaction)] = []
 		for target in targetGenes:
-			# again, only proteins
-			map[(sr,interaction)].append(target)
+			# add target interactions: don't duplicate
+			if target not in map[(sr,interaction)]:
+				map[(sr,interaction)].append(target)
 
 	return map
 
 # print out a 2-column interactions file of simple PPIs
 # protein -> protein
-edges = {}
+ppi_edges = {}
 tf_edges = {}
-
-# members in a complex get a different edge weight
-# parse past the protein and complex definitions
-complex_edges = collections.defaultdict(list)
-for line in open(options.pathway_file, 'r'):
-	type, name = line.rstrip().split("\t")
-	if type == "protein":
-		continue
-	elif type == "complex":
-		proteins = getGenes(name, componentMap, 1)
-		for combo in itertools.combinations(proteins, 2):
-			complex_edges[combo[0]].append(combo[1])
-	else:
-		break	
-			
+all_edges = {}
 
 # for each node, find all interactions, and use the component map to refine them to genes
 # if the interaction is a -t*, add it to the TF edge list, otherwise if -p* add it to the ppi edge list
-for node in nodes:
+for node in Interactions:
 
 	# complex, -t> or -t| interaction type
-	type =  nodes[node]
+	type = nodes[node]
 		
 	if node not in Interactions:
 		continue
 
 	for inode in Interactions[node]:
 
+		# type is a ';' separated list of the interactions
+		# for these node pairs -- may be member, component
+		# -t or -a or any mix of those
+
 		type = Interactions[node][inode]
-		if type.startswith("-t"):
-			tf_edges = addEdges(node, inode, type, tf_edges, componentMap) 
-		elif type.startswith("-a"):
-			edges = addEdges(node, inode, type, edges, componentMap) 
+		all_interactions = type.split(";")
+		for int in all_interactions:
+			# add to the all-edge map
+			all_edges = addEdges(node, inode, int, all_edges, componentMap) 
 
-interactions_out = open(options.ppi_output, 'w')
-for (source, interaction) in edges:
+# test: try writing all edges out
+all_out = open(options.flattened, 'w')
+for (source, interaction) in all_edges:
 
-	for sink in edges[(source, interaction)]:
-		interactions_out.write(source+"\t"+sink+"\t"+interaction+"\n")
+	for sink in all_edges[(source, interaction)]:
+		all_out.write(source+"\t"+sink+"\t"+interaction+"\n")
 
-interactions_out.close()
-			
-tf_out = open(options.tf_output, 'w')
-for (source, interaction) in tf_edges:
-
-	for sink in tf_edges[(source, interaction)]:
-		tf_out.write(source+"\t"+sink+"\t"+interaction+"\n")
-
-tf_out.close()
+all_out.close()
 
 c_out = open(options.component_output, 'w')
 for component in componentMap:
 	c_out.write(component+"\t"+" ".join(componentMap[component])+"\n")
 c_out.close()
-
-# protein edges
-
-edges = {}
-for p in Proteins:
-
-	# build edges
-	edges[p] = {}
-
-	if not p in Interactions:
-		continue
-
-	# add direct interactions
-	for inode in Interactions[p]:
-		genes = None
-		if complexRE.match(inode):
-			genes = getGenes(inode, componentMap, 1)
-		else:
-			genes = [ inode ]
-
-		for gene in genes:
-			if p == gene:
-				continue
-			edges[p][gene] = None
-
-# go over every interaction: 
-# map connections onto proteins
-for source in Interactions:
-	targets = Interactions[source]
-
-	# use to component map to find all the source genes
-	sourceGenes = None
-	if complexRE.match(source):
-		sourceGenes = getGenes(source, componentMap, 1)
-	else:
-		sourceGenes = [ source ]
-
-	# find target genes with the component map
-	targetGenes = []
-	for target in targets:
-		if complexRE.match(target):
-			for gene in getGenes(target, componentMap, 1):
-				targetGenes.append(gene)
-		else:
-			targetGenes.append(gene)
-
-	# connect these sink genes to the source gene
-	for sr in sourceGenes:
-
-		# only proteins here
-		if sr not in edges:
-			continue
-
-		for target in targetGenes:
-			# again, only proteins
-			if target in edges:
-				if sr == target:
-					continue
-				edges[sr][target] = None
-
-
-
-# print it out
-flattened_out = open(options.flattened, 'w')
-for source in edges:
-	for sink in sorted(edges[source]):
-		flattened_out.write(source+"\t"+sink+"\n")
-flattened_out.close()
-
-#for source in complex_edges:
-#
-#	for sink in complex_edges[source]:
-#		print source+"\t"+sink
-#
-
-# print a simple TF interactions file
