ee75c5aba317f2eae75ddbc03e7a8fda06afa82a
epaull <epaull@soe.ucsc.edu>
2013-01-18 14:22:58 -0800

    --Add edgeR implementation, analagous to sam.R -- add script to format to edgeR input (data matrix) with labels for tumor/normal from raw read count files

---
+++ R_shell/edgeR.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+
+# EDGER.R: 
+#  Runs the EdgeR algorithm on raw RNA-Seq counts data to compute the differential expression between
+#  two categories, defined by binary column labels. 
+#
+# For more information, see the edgeR tutorial that this script was adapted from 
+# http://cgrlucb.wikispaces.com/file/view/edgeR_Tutorial.pdf
+# 
+# Required Packages: edgeR
+#
+# Author: Evan Paull 
+# Date: Jan, 2013
+
+options(warn = -1)
+library('getopt')
+
+opt = getopt(matrix(c(
+    'help' , 'h', 1, "character",
+    'expr' , 'e', 1, "character",
+    'output' , 'o', 1, "character"
+	),ncol=4,byrow=TRUE));
+
+if (!is.null(opt$help) || is.null(opt$expr)) {
+	self = commandArgs()[1];
+	#print a friendly message and exit with a non-zero error code
+	cat(paste("Usage: ",self,"  --expr <expression file> \n"))
+	q();
+}
+
+library(edgeR)
+
+raw.data <- read.delim(opt$expr, sep="\t", header=TRUE, row.names=1)
+counts <- raw.data[2:nrow(raw.data),]
+groups <- as.numeric(raw.data[1,])
+
+# create DGE List object from counts, assignments
+cds <- DGEList(counts, group = groups)
+# filter out low count reads, impossible to detect in DE. Keep only genes with at least 1 read per million in at least 3 samples. 
+# calculate the normalization factors which correct for the compositions of each sample. 
+cds <- cds[rowSums(1e+06 * cds$counts/expandAsMatrix(cds$samples$lib.size, dim(cds)) > 1) >= 3, ]
+cds <- calcNormFactors( cds )
+cds <- estimateCommonDisp( cds )
+
+# estimate tagwise dispersion 50(#samples - #groups)
+cds <- estimateTagwiseDisp(cds)
+
+# get list using tagwise (gene) dispersion estimates
+de.tgw <- exactTest( cds , dispersion="tagwise" , pair = c( "0" , "1" ) )
+
+# sort results by logpval
+resultsByFC.tgw <- topTags( de.tgw , n = nrow( de.tgw$table ) , sort.by = "p.value" )$table
+
+write.table(resultsByFC.tgw, file=opt$output, sep="\t",quote=FALSE)
